519e6afdbc5f762b1b0c43302f283622c4a2967f
zjun <565061763@qq.com>
2019-04-26 10:42:55 +0800

    FStateLifecycleAdapter

--- lib/src/ext/state_lifecycle_adapter.dart
+++ lib/src/ext/state_lifecycle_adapter.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 class FStateLifecycleAdapter implements FLifecycleOwner, FStateLifecycle {
   final FLifecycleRegistry _lifecycleRegistry;
   bool _started;
+  bool _startedMarker;
 
   FStateLifecycleAdapter({FLifecycleRegistry lifecycleRegistry})
       : this._lifecycleRegistry =
@@ -22,7 +23,12 @@ class FStateLifecycleAdapter implements FLifecycleOwner, FStateLifecycle {
 
   @override
   Widget build(BuildContext context) {
-    if (_started == null) {
+    if (_startedMarker == null) {
+      _startedMarker = true;
+    }
+
+    if (_startedMarker) {
+      _startedMarker = false;
       _started = true;
       _notifyStartOrStop();
     }
@@ -30,13 +36,22 @@ class FStateLifecycleAdapter implements FLifecycleOwner, FStateLifecycle {
 
   @override
   void deactivate() {
-    _started = !_started;
-    _notifyStartOrStop();
+    assert(_startedMarker = false);
+
+    final bool expected = !_started;
+    if (expected) {
+      _startedMarker = true;
+      // 等待build
+    } else {
+      _started = false;
+      _notifyStartOrStop();
+    }
   }
 
   @override
   void dispose() {
     _started = null;
+    _startedMarker = null;
     _lifecycleRegistry.handleLifecycleEvent(FLifecycleEvent.onDestroy);
   }
 
