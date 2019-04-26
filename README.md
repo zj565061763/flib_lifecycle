69eea16632e22f5ef636a96fdf0877bcb4362862
zjun <565061763@qq.com>
2019-04-26 11:04:34 +0800

    去掉 FStateLifecycleAdapter

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
   flib_lifecycle:
     git:
       url: git://github.com/zj565061763/flib_lifecycle
-      ref: 1.0.0-rc12
+      ref: 1.0.0-rc13
 ```
--- lib/flib_lifecycle.dart
+++ lib/flib_lifecycle.dart
@@ -1,6 +1,5 @@
 library flib_lifecycle;
 
 export 'src/ext/lifecycle_wrapper.dart';
-export 'src/ext/state_lifecycle_adapter.dart';
 export 'src/lifecycle.dart';
 export 'src/lifecycle_impl.dart';
